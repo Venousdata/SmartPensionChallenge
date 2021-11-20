16ce8f21e69ced649e66f1f2914ec042984b1148
James Howell <jammyboy122@hotmail.co.uk>
2021-11-20 14:44:08 +0000

    Slap together a table quickly to display data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,69 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { get, post } from "./api/api";
+import { post } from "./api/api";
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@material-ui/core";
 
 export default function App() {
   const [sortBy, setSortBy] = useState("view");
-  const [order, setOrder] = useState("desc");
-  const [tableData, setTableData] = useState([]);
+  const [ascDesc, setAscDesc] = useState(false);
+  const [tableRows, setTableRows]: any[] = useState([]);
+  const tableHeaders = ["path", "view", "uniqueView"];
 
   useEffect(() => {
-    post("webpageviews", { sortBy, order }).then((data) => {
-      setTableData(data || []);
-    });
-  }, [sortBy, order]);
+    post("webpageviews", { sortBy, order: ascDesc ? "asc" : "desc" }).then(
+      (data) => {
+        setTableRows(data || []);
+      }
+    );
+  }, [sortBy, ascDesc]);
 
-  console.log("==tableData==", tableData);
+  const sortByHeader = (header: string, ascDesc: boolean) => {
+    setSortBy(header);
+    setAscDesc(ascDesc);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableContainer component={Paper}>
+        <Table style={{ minWidth: 650 }}>
+          <TableHead>
+            <TableRow>
+              {tableHeaders.map((header, idx) => (
+                <TableCell key={idx}>
+                  {header}
+                  {header !== "path" && (
+                    <button
+                      key={idx}
+                      onClick={() => sortByHeader(header, !ascDesc)}
+                    >
+                      ascDesc
+                    </button>
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableRows.map((row: any, idx: number) => (
+              <TableRow key={idx} style={{ border: 0 }}>
+                <TableCell component="th" scope="row">
+                  {row.path}
+                </TableCell>
+                <TableCell>{row.view}</TableCell>
+                <TableCell>{row.uniqueView}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
