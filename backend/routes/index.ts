db251d1fe2fc5b86ce27557d03718d6c97a3dd22
James Howell <jammyboy122@hotmail.co.uk>
2021-11-20 14:48:37 +0000

    remove next arg

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import { getWebsiteViews } from "../controllers/webpage_controller";
 
 export default (app: Express): void => {
-  app.post("/webpageviews", async ({ body: { sortBy, order } }, res, next) => {
+  app.post("/webpageviews", async ({ body: { sortBy, order } }, res) => {
     try {
       const data = await getWebsiteViews("./data/webserver.log", sortBy, order);
       res.send(data);
